c48bdf831067cc96e77376f669f1cb4257abd2a6
futomakiyoin <futomakiyoin@gmail.com>
2021-05-17 23:22:26 -0400

    Fixed function running before timer

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
-from geeksforgeeks.timsort import *
-from geeksforgeeks.quicksort import *
-from geeksforgeeks.mergesort import *
+from geeksforgeeks.binaryinsertionsort import *
+from geeksforgeeks.bubblesort import *
 from geeksforgeeks.heapsort import *
+from geeksforgeeks.mergesort import *
+from geeksforgeeks.quicksort import *
+from geeksforgeeks.timsort import *
+
 import time
 import random
 
 # Edit array here
 array = random.sample(range(0, 10000), 10000)
 # quicksort breaks with this
-# ["Quicksort", quicksort(array)]
+# ["quicksort",quick_sort(array)]
 
 funcList = [
-    ["sorted()", sorted(array)],
-    ["Heapsort", heapSort(array)],
-    ["Mergesort", mergeSort(array)],
-    ["Timsort", timSort(array)],
+    ["binaryinsertionsort", binary_insertion_sort],
+    ["bubblesort", bubbleSort],
+    ["heapsort", heapSort],
+    ["mergesort", mergeSort],
+    ["timsort", timSort],
 ]
 
+print("here")
 for f in funcList:
     temparray = array.copy()
     start_time = time.time()
     time.sleep(1)
-    f[1]
+    f[1](array)
     print(f[0], (time.time() - start_time - 1))
